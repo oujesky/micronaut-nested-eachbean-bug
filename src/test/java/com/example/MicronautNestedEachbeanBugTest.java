b10cdd05fcec730f53fb7f9bd3d35cadc4ab0390
Miroslav Oujeský <oujesky@users.noreply.github.com>
2025-01-29 09:33:51 +0100

    Fresh context for each test

--- src/test/java/com/example/MicronautNestedEachbeanBugTest.java
+++ src/test/java/com/example/MicronautNestedEachbeanBugTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@MicronautTest(startApplication = false)
+@MicronautTest(startApplication = false, rebuildContext = true)
 class MicronautNestedEachbeanBugTest {
 
     @Inject
